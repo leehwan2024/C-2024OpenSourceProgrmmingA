cbc423e8547bc251f814be7fd9e81468261ad749
hwan <202444010@itc.ac.kr>
2024-10-04 10:58:07 +0900

    week06.1

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
 
 	i := 13
-	f := 12.9
-
+	var f float64 = 12.9
+	c1 := '김'
+	c2 := 'b'
 	fmt.Printf("Value i : %d\n value f : %f", i, f)
-	fmt.Printf("%d * %f = %f", i, f, float64(i)*f)
+	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
+
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeFor(c1).Align, reflect.TypeOf(c2))
 }
