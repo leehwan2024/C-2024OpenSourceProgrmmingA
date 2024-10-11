1d0d6f5b27e73ed47cce8f5987f947e0db843299
hwan <202444010@itc.ac.kr>
2024-10-11 14:46:09 +0900

    v0.4

--- week07/main.go
+++ week07/main.go
@@ -5,13 +5,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Input your name")
-	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err, log.Fatal(err))
+	fmt.Print("점수 입력: ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
 
+	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i의 값을 정수형(32비트)로 변환, 입력받은 값-> 16진수로변환
+	//                         실수형 받고 싶으면 ParseFloat(i,32) ㄱㄱ
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d", score)
+
+	} else {
+		fmt.Println("BCDE")
+		fmt.Printf("%d", score)
+	}
 }
