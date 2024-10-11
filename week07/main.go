c09949c646a193f33993ff7c948df73e39894b43
hwan <202444010@itc.ac.kr>
2024-10-11 14:55:09 +0900

    v0.4

--- week07/main.go
+++ week07/main.go
@@ -11,7 +11,9 @@ func main() {
 	in := bufio.NewReader(os.Stdin)
 	fmt.Println("Input your name")
 	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err, log.Fatal(err))
-
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
