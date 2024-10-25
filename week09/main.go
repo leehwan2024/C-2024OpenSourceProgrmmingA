5c041b8368a484193ac2f17c7b07879d320f38ad
hwan <202444010@itc.ac.kr>
2024-10-25 13:09:08 +0900

    v0.7_func

--- week09/main.go
+++ week09/main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+func combie(i float64, j float64) float64 {
+	a := i * j
+	return a
+}
 func main() {
-	var result string
-	result = fmt.Sprintf("%0.2f 나누기 %0.2f는  %0.2f\n", 1.0, 3.0, 1.0/3.0)
-	fmt.Println(result)
+	var a, b float64
+	a = combie(6.5, 8.6)
+	b = combie(3.5, 1.6)
+	fmt.Printf("%f\n %f\n", a, b)
 }
