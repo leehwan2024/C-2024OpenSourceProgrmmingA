8c97f1fd027d30bf712e27ed416b991e8c469c3b
hwan <202444010@itc.ac.kr>
2024-10-25 10:45:06 +0900

    v0.2

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
-	"time"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	rand.Seed(time.Now.Unix())
-	target := rand.Intn(2) + 1
-	fmt.Printf("%d\n", target)
+
+	answer := rand.Intn(100) + 1
+	fmt.Printf("%d\n", answer)
+
+	fmt.Print("점수 입력: ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	guess, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
+
+	if answer == guess {
+		fmt.Println("정답!")
+	} else if answer > guess {
+		fmt.Println("입력 수가 정답보다 작습니다")
+	} else if answer < guess {
+		fmt.Println("입력 수가 정답보다 큽니다")
+	}
+	fmt.Printf("정답은 %d", answer)
+
 }
