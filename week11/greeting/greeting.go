9dc016d0572fa319e865cc2811f93d66fd31f27c
hwan <202444010@itc.ac.kr>
2024-11-14 17:10:25 +0900

    v1.4 pakage

--- week11/greeting/greeting.go
+++ week11/greeting/greeting.go
@@ -1,14 +1,23 @@
-package greeting
+package keyboard
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func hi(name string) {
-	fmt.Printf("Hi %s!\n", name)
-}
-func hello(name string) {
-	fmt.Printf("Hello %s!\n", name)
-}
-func AllGreetings(name string) {
-	hello(name)
-	hi(name)
+func GetInteger() (int, error) {
+	r := bufio.NewReader(os.Stdin)
+	a, err := r.ReadString('\n')
+	if err != nil {
+		//log.Fatal(err)
+		return 0, err
+	}
+	a = strings.TrimSpace(a)
+	n, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	return n, nil
 }
