ab271d46d7d8e07c592ed6cff83c046c971b0ed3
hwan <202444010@itc.ac.kr>
2024-11-15 10:52:21 +0900

    v1.7 array literal

--- week12/main.go
+++ week12/main.go
@@ -9,6 +9,11 @@ func main() {
 	var dates = [3]time.Time{time.Unix(1, 0),
 		time.Unix(12, 0),
 		time.Unix(1735244111, 0)}
-	fmt.Println(dates[0], dates[1], dates[2])
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, "번째 배열", dates[i])
+	}
+	// fmt.Printf("%v\n", dates)
+	// fmt.Println(dates[0], dates[1], dates[2])
 
 }
