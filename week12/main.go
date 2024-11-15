929d4f16e06318da47abd0c4bf94a04d7d1015ca
hwan <202444010@itc.ac.kr>
2024-11-15 11:42:31 +0900

    v2.0 array

--- week12/main.go
+++ week12/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	var dates = [3]time.Time{time.Unix(1, 0),
+
+	var gpa [3]float64
+	for i := 0; i < len(gpa); i++ {
+		fmt.Println("Input float number:")
+		gpa[i], _ = keyboard.GetFloat()
+	}
+	for i, v := range gpa {
+		fmt.Printf("%v: %f\n", i, v)
+	}
+	/*var dates = [3]time.Time{time.Unix(1, 0),
 		time.Unix(12, 0),
 		time.Unix(1735244111, 0)}
 
 	for i, _ := range dates {
 		fmt.Println(i)
-	}
+	}*/
 	//for i := 0; i < len(dates); i++ {
 	//	fmt.Println(i, "번째 배열", dates[i])
 	//}
