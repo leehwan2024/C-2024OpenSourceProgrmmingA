7adcc85a0ed89cf5b476b4cb975de28124796ec5
hwan <202444010@itc.ac.kr>
2024-11-15 10:45:41 +0900

    11

--- week12/main.go
+++ week12/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var dates = [3]time.Time{time.Unix(1, 0),
 		time.Unix(12, 0),
-		time.Unix(1976299143, 0)}
+		time.Unix(1735244111, 0)}
 	fmt.Println(dates[0], dates[1], dates[2])
 
 }
