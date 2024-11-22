260e0ee84ffafe2c2d151020ae013595d9e45178
hwan <202444010@itc.ac.kr>
2024-11-22 14:10:10 +0900

    v2.6 slice

--- week13/main.go
+++ week13/main.go
@@ -1,17 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func test(strs ...string) {
+	fmt.Println(strs)
+}
 
 func main() {
-	var empthySlice []int
-	//empthySlice = make([]int, 5)
-	if len(empthySlice) == 0 {
-		empthySlice = append(empthySlice, 77)
-	}
-	fmt.Println(empthySlice)
-	var gpa [5]float64 = [5]float64{3.5, 4.1, 3.9, 4.23}
-	gpa_slice := gpa[1:4]
-	gpa_slice[2] = 2.71
-	gpa_slice = append(gpa_slice, 4.3)
-	fmt.Println(gpa_slice, gpa)
+	slice := os.Args[1:]
+	fmt.Println(slice[2])
+	fmt.Printf("%T\n", slice[2])
+	slice = append(slice, "inha")
+	fmt.Println(slice)
+	test("123", "456")
 }
