691d5c086a6c39216f7fc612e8da3cbc9e04db6d
hwan <202444010@itc.ac.kr>
2024-11-22 11:30:28 +0900

    v2.4 slice

--- week13/main.go
+++ week13/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var empthySlice []int
+	empthySlice = make([]int, 5)
+	fmt.Println(empthySlice)
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 3.9, 4.23}
 	gpa_slice := gpa[1:4]
 	gpa_slice[2] = 2.71
