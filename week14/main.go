e16f7f634568a125a032bdfb09cd20c547a20c67
hwan <202444010@itc.ac.kr>
2024-11-29 11:32:11 +0900

    2.9 struct

--- week14/main.go
+++ week14/main.go
@@ -2,22 +2,20 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int64
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int64
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 202444010
 	student1.name = "honggildong"
 	student1.gpa = 3.9
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int64
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 2024440030
 	student2.name = "choijihu"
 	student2.gpa = 3.5
